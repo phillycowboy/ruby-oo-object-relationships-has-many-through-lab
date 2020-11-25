30840a27c6572719e12784f325946cd61e06c66e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-24 22:01:35 -0500

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
 
+class Genre 
+
+    @@all = []
+    attr_accessor :name
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end
+    
+    def self.all 
+        @@all 
+    end
+
+    def songs 
+        Song.all do |songs|
+            songs.genre == self 
+        end
+    end
+
+    def artists 
+        songs.collect do |songs|
+            songs.artist 
+        end
+    end
+
+end
